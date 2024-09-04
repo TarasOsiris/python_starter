ef5230263af973d7efd3772444d8f5659be4f035
Taras Leskiv <leskiv.taras@gmail.com>
2024-09-05 00:02:32 +0300

    add exercises 10 and 11

---
+++ workbook/10_arithmetic.py
@@ -0,0 +1,12 @@
+﻿from math import log10
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(f"Sum is {a + b}")
+print(f"Difference is {b - a}")
+print(f"Product is {b * a}")
+print(f"Quotient is {b / a}")
+print(f"Remainder is {b % a}")
+print(f"log10a is {log10(a)}")
+print(f"a pow b is {a ** b}")
---
+++ workbook/11_fuel_efficiency.py
@@ -0,0 +1,8 @@
+﻿efficiency_mpg = float(input("Enter fuel efficiency in MPG: "))  # miles per gallon
+
+KM_PER_MILE = 1.60934
+LITRE_PER_GALLON = 3.78541
+# See https://www.mpgtolitres.com/ for the explanation
+efficiency_l_per_km = 100 / (efficiency_mpg * KM_PER_MILE / LITRE_PER_GALLON)  # km per litre
+
+print(f"fuel efficiency is {efficiency_l_per_km:.2f} L/100km")
