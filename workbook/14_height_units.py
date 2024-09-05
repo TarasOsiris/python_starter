7f25ea3434ae23f1f7defb786b699caac28089d5
Taras Leskiv <leskiv.taras@gmail.com>
2024-09-05 23:31:59 +0300

    14

---
+++ workbook/14_height_units.py
@@ -0,0 +1,7 @@
+﻿feet = float(input("Enter your height in feet: "))
+inches = float(input("Enter your height in inches: "))
+
+IN_PER_FT = 12
+CM_PER_IN = 2.54
+
+print(f"Your height is {(feet * IN_PER_FT + inches) * CM_PER_IN} cm")
