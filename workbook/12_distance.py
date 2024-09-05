31be73660d5cd9804dc403e40756693dd006024c
Taras Leskiv <leskiv.taras@gmail.com>
2024-09-05 22:59:33 +0300

    ex 12,13

---
+++ workbook/12_distance.py
@@ -0,0 +1,12 @@
+﻿from math import radians
+from cmath import *
+
+lat1 = radians(float(input("Enter Latitude 1: ")))
+lng1 = radians(float(input("Enter Longitude 2: ")))
+
+lat2 = radians(float(input("Enter Latitude 2: ")))
+lng2 = radians(float(input("Enter Longitude 2: ")))
+
+distance = 6371.01 * acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lng1 - lng2))
+
+print(distance)
---
+++ workbook/13_making_change.py
@@ -0,0 +1,14 @@
+﻿cents = int(input("Enter the number of cents: "))
+
+CENTS_PER_PENNY = 1
+CENTS_PER_NICKEL = 5
+CENTS_PER_DIME = 10
+CENTS_PER_QUARTER = 25
+CENTS_PER_LOONIE = 100
+CENTS_PER_TOONIE = 200
+
+print(" ", cents // CENTS_PER_TOONIE, "toonies")
+cents = cents % CENTS_PER_TOONIE
+
+print(" ", cents // CENTS_PER_LOONIE, "loonies")
+cents = cents % CENTS_PER_LOONIE
